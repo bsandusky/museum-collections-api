b579eb62aaf1907696a00674a87f36d230695b57
Brett Sandusky <brett.sandusky@gmail.com>
2017-02-13 01:14:06 +0000

    update server

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 'use strict';
 
 const Hapi = require('hapi');
-
-// Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
     port: 8000
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: (request, reply) => {
+server.register(require('inert'), (err) => {
 
-        return reply('hello world');
+    if (err) {
+        throw err;
     }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/index.html');
+        }
+    });
 });
 
-// Start the server
 server.start((err) => {
 
     if (err) {
